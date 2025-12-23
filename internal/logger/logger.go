ba8aecd3bd0619a46063eaf000ee4fe5615dab65
riktikdev <dibrova.artur08@gmail.com>
2025-12-23 23:15:08 +0100

    fix(logger): add fallback logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -27,6 +27,10 @@ func SetupLogger(env string) *slog.Logger {
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
 	}
 
 	return log
